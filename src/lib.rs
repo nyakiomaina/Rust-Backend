fa09285e67a29b9724365bed3390dbed96f7ed15
nyakiomaina <carolinenyakio21@gmail.com>
2025-03-09 17:30:22 +0300

    Working With HTML Forms

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,30 @@
-// use actix_web::{web, App, HttpResponse, HttpServer};
-// async fn health_check() -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
-// 
-// pub async fn run() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .route("/health_check", web::get().to(health_check))
-//     })
-//         .bind("127.0.0.1:8000")?
-//         .run()
-//         .await
-// }
-
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
-    
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }
